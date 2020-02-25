b9ee7b54c646b734dcfcfc4a385d1325f979049e
Maxim Yakovenko <yakovenko_me@km-union.ru>
2020-02-25 16:16:39 +0300

    Remove email settings.

--- config/settings/test.py
+++ config/settings/test.py
@@ -5,8 +5,3 @@ class Test(BaseConfiguration):
 
     # Debug
     DEBUG = True
-
-    # Email
-    EMAIL_HOST = "localhost"
-    EMAIL_PORT = 1025
-    EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
