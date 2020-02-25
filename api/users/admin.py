bc6b0bfcd08193572226c068653f340c2c471b57
Maxim Yakovenko <yakovenko_me@km-union.ru>
2020-02-25 16:49:30 +0300

    Redeclare User model admin. Add Account model admin.

--- api/users/admin.py
+++ api/users/admin.py
@@ -1,7 +1,34 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.admin import UserAdmin as UserAdminBase
+from django.utils.translation import gettext_lazy as _
 
 from . import models
 
 
-admin.site.register(models.User, UserAdmin)
+@admin.register(models.User)
+class UserAdmin(UserAdminBase):
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        (_("Personal info"), {"fields": ("email",)}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+    list_display = ("username", "email", "is_bot", "is_staff")
+    search_fields = ("username", "email")
+
+
+@admin.register(models.Account)
+class AccountAdmin(admin.ModelAdmin):
+    list_display = ("email", "created")
+    search_fields = ("email",)
