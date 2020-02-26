39e374eb8724c92b7dfbec5cb8bc68c99eb70510
Maxim Yakovenko <yakovenko_me@km-union.ru>
2020-02-26 15:29:16 +0300

    Add TitleSlug abstract model.

--- api/utils/models.py
+++ api/utils/models.py
@@ -1,6 +1,9 @@
 import uuid
 
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+from django_extensions.db.fields import AutoSlugField
 
 
 class UUIDModel(models.Model):
@@ -9,6 +12,28 @@ class UUIDModel(models.Model):
     """
 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    class Meta:
+        abstract = True
+
+
+class TitleSlugModel(models.Model):
+    """
+    TitleSlugModel
+
+    An abstract base class model that provides title field and
+    a self-managed "slug" field that populates from the title.
+
+    .. note ::
+        If you want to use custom "slugify" function, you could
+        define ``slugify_function`` which then will be used
+        in :py:class:`AutoSlugField` to slugify ``populate_from`` field.
+
+        See :py:class:`AutoSlugField` for more details.
+    """
+
+    title = models.CharField(_("title"), max_length=255)
+    slug = AutoSlugField(_("slug"), populate_from="title")
 
     class Meta:
         abstract = True
