f90b0088eb47e9f8c720e53405c28d3b430dfdfb
Maxim Yakovenko <yakovenko_me@km-union.ru>
2020-02-27 11:54:57 +0300

    Add ref_name for User serializer meta.

--- api/users/serializers.py
+++ api/users/serializers.py
@@ -5,5 +5,6 @@ from rest_framework import serializers
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
+        ref_name = "api.users.serializers.UserSerializer"
         model = get_user_model()
         fields = ("pk", "username", "email", "first_name", "last_name")
