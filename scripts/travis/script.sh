50ee7d691dc9c5a04ba4d053559c075f5a490dd1
Maxim Yakovenko <yakovenko_me@km-union.ru>
2020-02-27 12:06:50 +0300

    Start test runner locally.

--- scripts/travis/script.sh
+++ scripts/travis/script.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker-compose --version
-docker-compose -f docker-compose.test.yml build
-docker-compose -f docker-compose.test.yml run --rm web scripts/travis/run_tests.sh
+docker-compose -f docker-compose.yml up -d
+scripts/travis/run_tests.sh
