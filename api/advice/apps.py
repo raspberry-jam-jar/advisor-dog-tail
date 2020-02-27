d2dcf2a67c0c272803ab92e1fa8aaf085aae20ea
Maxim Yakovenko <yakovenko_me@km-union.ru>
2020-02-27 15:22:57 +0300

    Connect signal handlers.

--- api/advice/apps.py
+++ api/advice/apps.py
@@ -1,5 +1,10 @@
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 
 
 class AdviceConfig(AppConfig):
     name = "advice"
+    verbose_name = _('advice')
+
+    def ready(self):
+        from . import receivers # noqa: F401
