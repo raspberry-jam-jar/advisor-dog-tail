bc139bd21da3fbd6c6eb0bf17e1cc53914d3b857
Maxim Yakovenko <yakovenko_me@km-union.ru>
2020-02-27 15:21:27 +0300

    Add Tag model. Connect tag model to advice one.

--- api/advice/models.py
+++ api/advice/models.py
@@ -6,12 +6,26 @@ from model_utils.models import TimeStampedModel
 from api.utils.models import UUIDModel, TitleSlugModel
 
 
+class Tag(UUIDModel, TitleSlugModel, TimeStampedModel):
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = _('tag')
+        verbose_name_plural = _('tags')
+        indexes = (
+            models.Index(fields=('title',)),
+        )
+        unique_together = ('title', 'slug')
+
 class Advice(UUIDModel, TitleSlugModel, TimeStampedModel):
     """
     An advice model for storing usefull links.
     """
 
     link = models.URLField(_("uri link"))
+    tags = models.ManyToManyField(Tag, blank=True, verbose_name=_('advice tags'))
 
     def __str__(self):
         return self.title
