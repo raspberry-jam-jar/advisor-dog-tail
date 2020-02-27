cb5b1dbde4e1c9ab0530a15ab9524d65b8b74501
Maxim Yakovenko <yakovenko_me@km-union.ru>
2020-02-27 15:24:00 +0300

    Add Tag model factory.

--- api/advice/tests/factories.py
+++ api/advice/tests/factories.py
@@ -1,15 +1,27 @@
+import factory
+
 from factory import django as django_factory
 from factory import faker
 
-from ..models import Advice
+from ..models import Advice, Tag
 
+
+class TagFactory(django_factory.DjangoModelFactory):
+    """
+    Tag model factory
+    """
+
+    title = faker.Faker('slug')
+    class Meta:
+        model = Tag
+        django_get_or_create = ('title',)
 
 class AdviceFactory(django_factory.DjangoModelFactory):
     """
     Advice model factory.
     """
 
-    title = faker.Faker("name")
+    title = faker.Faker("slug")
     link = faker.Faker("uri")
 
     class Meta:
