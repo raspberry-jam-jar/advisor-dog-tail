ec5e5714fbcb81f9e45a653d8532b5009ed02708
Maxim Yakovenko <yakovenko_me@km-union.ru>
2020-02-28 11:35:47 +0300

    Add Tag model serializer. Add tags property to Advice serializer.

--- api/advice/serializers.py
+++ api/advice/serializers.py
@@ -1,6 +1,31 @@
-from rest_framework import serializers
+from collections import OrderedDict
+from collections.abc import Mapping
 
-from .models import Advice
+from django.core.exceptions import ValidationError as DjangoValidationError
+from django.db import models
+
+from rest_framework.settings import api_settings
+from rest_framework import serializers
+from rest_framework.utils import model_meta
+from rest_framework.serializers import raise_errors_on_nested_writes, as_serializer_error
+from rest_framework.exceptions import ErrorDetail, ValidationError
+from rest_framework.fields import get_error_detail, set_value
+from rest_framework.fields import (
+    CreateOnlyDefault, CurrentUserDefault, SkipField, empty
+)
+from .models import Tag, Advice
+
+
+class TagSerializer(serializers.ModelSerializer):
+    """
+    Advice tag serializer.
+    """
+
+    class Meta:
+        model = Tag
+        fields = ('title','slug',)
+        read_only_fields = ('slug',)
+
 
 
 class AdviceSerializer(serializers.ModelSerializer):
@@ -8,7 +33,44 @@ class AdviceSerializer(serializers.ModelSerializer):
     Advice model serializer.
     """
 
+    tags = TagSerializer(
+        many=True,
+    )
+
     class Meta:
         model = Advice
-        fields = ("title", "slug", "link", "created")
+        fields = ("title", "slug", 'tags', "link", "created")
         read_only_fields = ("slug", "created")
+
+    def update_tags(sefl, manager, values: list):
+        for row in values:
+            tag, _ = Tag.objects.get_or_create(title=row['title'])
+            manager.add(tag)
+        return manager
+
+    def update(self, instance, validated_data):
+        info = model_meta.get_field_info(instance)
+
+        # Simply set each attribute on the instance, and then save it.
+        # Note that unlike `.create()` we don't need to treat many-to-many
+        # relationships as being a special case. During updates we already
+        # have an instance pk for the relationships to be associated with.
+        m2m_fields = []
+        for attr, value in validated_data.items():
+            if attr in info.relations and info.relations[attr].to_many:
+                m2m_fields.append((attr, value))
+            else:
+                setattr(instance, attr, value)
+
+        instance.save()
+
+        # Note that many-to-many fields are set after updating instance.
+        # Setting m2m fields triggers signals which could potentially change
+        # updated instance and we do not want it to collide with .update()
+        for attr, value in m2m_fields:
+            field = getattr(instance, attr)
+            # if the field is an instance of a related manager which has Tag model               
+            if field.model and field.model == Tag:
+                field = self.update_tags(field, value)
+
+        return instance
