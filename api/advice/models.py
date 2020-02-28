80d402d2b10b192b9a94eb6cefdc86f6ce84838b
Maxim Yakovenko <yakovenko_me@km-union.ru>
2020-02-28 11:20:10 +0300

    Add Mapping table between advice and tag model instances.

--- api/advice/models.py
+++ api/advice/models.py
@@ -29,6 +29,11 @@ class Advice(UUIDModel, TitleSlugModel, TimeStampedModel):
         "users.Account",
         on_delete=models.PROTECT,
         verbose_name=_("author of the advice"),
+    tags = models.ManyToManyField(
+        Tag, 
+        blank=True, 
+        through='Mapping',
+        verbose_name=_("advice's tags")
     )
 
     def __str__(self):
@@ -37,3 +42,19 @@ class Advice(UUIDModel, TitleSlugModel, TimeStampedModel):
     class Meta:
         verbose_name = _("advice")
         verbose_name_plural = _("advices")
+
+
+class Mapping(UUIDModel, TimeStampedModel):
+    """
+    Mapping entry between advice and tag model instance
+    """
+    tag = models.ForeignKey('Tag', on_delete=models.CASCADE)
+    advice = models.ForeignKey('Advice', on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = _("advice's tags")
+        verbose_name_plural = _("advices' tags")
+        unique_together = ('tag', 'advice')
+        indexes = (
+            models.Index(fields=('tag','advice')),
+        )
