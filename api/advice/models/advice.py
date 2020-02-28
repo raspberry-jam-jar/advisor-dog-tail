e4ee35d5c6e8804d9ae1996afae4d820af019a22
Maxim Yakovenko <yakovenko_me@km-union.ru>
2020-02-28 16:40:14 +0300

    Separate tag-related models from advice-related one.

--- api/advice/models/advice.py
+++ api/advice/models/advice.py
@@ -5,23 +5,12 @@ from model_utils.models import TimeStampedModel
 
 from api.utils.models import UUIDModel, TitleSlugModel
 
+from .tag import Tag
 
-class Tag(UUIDModel, TitleSlugModel, TimeStampedModel):
-
-    def __str__(self):
-        return self.title
-
-    class Meta:
-        verbose_name = _('tag')
-        verbose_name_plural = _('tags')
-        indexes = (
-            models.Index(fields=('title',)),
-        )
-        unique_together = ('title', 'slug')
 
 class Advice(UUIDModel, TitleSlugModel, TimeStampedModel):
     """
-    An advice model for storing usefull links.
+    An advice model for storing useful links.
     """
 
     link = models.URLField(_("uri link"))
@@ -30,10 +19,7 @@ class Advice(UUIDModel, TitleSlugModel, TimeStampedModel):
         on_delete=models.PROTECT,
         verbose_name=_("author of the advice"),
     tags = models.ManyToManyField(
-        Tag, 
-        blank=True, 
-        through='Mapping',
-        verbose_name=_("advice's tags")
+        Tag, blank=True, through="Mapping", verbose_name=_("advice's tags")
     )
 
     def __str__(self):
@@ -48,13 +34,12 @@ class Mapping(UUIDModel, TimeStampedModel):
     """
     Mapping entry between advice and tag model instance
     """
-    tag = models.ForeignKey('Tag', on_delete=models.CASCADE)
-    advice = models.ForeignKey('Advice', on_delete=models.CASCADE)
+
+    tag = models.ForeignKey("advice.Tag", on_delete=models.CASCADE)
+    advice = models.ForeignKey("advice.Advice", on_delete=models.CASCADE)
 
     class Meta:
         verbose_name = _("advice's tags")
         verbose_name_plural = _("advices' tags")
-        unique_together = ('tag', 'advice')
-        indexes = (
-            models.Index(fields=('tag','advice')),
-        )
+        unique_together = ("tag", "advice")
+        indexes = (models.Index(fields=("tag", "advice")),)
