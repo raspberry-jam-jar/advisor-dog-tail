05e4df827056254ea7c3a2f2e2c8ff480e4d257f
Maxim Yakovenko <yakovenko_me@km-union.ru>
2020-02-28 17:09:28 +0300

    Fix imports. Add test cases for tag-related actions.

--- api/advice/tests/test_viewsets.py
+++ api/advice/tests/test_viewsets.py
@@ -8,9 +8,10 @@ from rest_framework.test import APIRequestFactory, force_authenticate
 from model_bakery import baker
 
 from ..viewsets import AdviceViewSet
-from ..models import Advice
+from ..models.advice import Advice
+from ..models.tag import Tag
 
-from .factories import AdviceFactory
+from .factories import AdviceFactory, TagFactory
 
 
 # Create your tests here.
@@ -42,9 +43,9 @@ class TestAdviceViewset:
 
         new_advice = AdviceFactory.build()
         advice_data = {"title": new_advice.title, "link": new_advice.link}
-        request = request_factory.put(f"/advices/{advice.pk}/", advice_data)
+        request = request_factory.put(f"/advices/{advice.slug}/", advice_data)
         force_authenticate(request, user=django_user)
-        response = advice_vs.as_view({"put": "update"})(request, pk=advice.pk)
+        response = advice_vs.as_view({"put": "update"})(request, slug=advice.slug)
         assert response.status_code == HTTPStatus.OK
         assert Advice.objects.filter(title=new_advice.title).exists()
 
@@ -62,11 +63,11 @@ class TestAdviceViewset:
                 advice = baker.make(Advice)
                 value = getattr(new_advice, field)
                 request = request_factory.patch(
-                    f"/advices/{advice.pk}/", {field: value}
+                    f"/advices/{advice.slug}/", {field: value}
                 )
                 force_authenticate(request, user=django_user)
                 response = advice_vs.as_view({"patch": "partial_update"})(
-                    request, pk=advice.pk
+                    request, slug=advice.slug
                 )
                 lookup_data = {field: value}
                 assert response.status_code == HTTPStatus.OK
@@ -95,3 +96,94 @@ class TestAdviceViewset:
         assert response.status_code == HTTPStatus.OK
         assert response.data["count"] == 1
         assert response.data["results"][0]["slug"] == usefull_advice.slug
+
+    def test_remove_advice_tag_by_slug(
+        self,
+        db,
+        request_factory: APIRequestFactory,
+        django_user: get_user_model(),
+        advice: Advice,
+        advice_vs: AdviceViewSet,
+    ):
+        video_tag = TagFactory(title="video")
+        advice.tags.add(video_tag)
+        request = request_factory.delete(
+            f"/advices/{advice.slug}/tag/{video_tag.slug}/"
+        )
+        force_authenticate(request, user=django_user)
+        response = advice_vs.as_view({"delete": "tag"})(
+            request, slug=advice.slug, tag_slug=video_tag.slug
+        )
+
+        assert response.status_code == HTTPStatus.NO_CONTENT
+        assert not advice.tags.filter(title=video_tag.title).exists()
+
+    def test_search_advice_by_tag_title(
+        self,
+        db,
+        subtests,
+        request_factory: APIRequestFactory,
+        django_user: get_user_model(),
+        user_tag: Tag,
+        advice_vs: AdviceViewSet,
+    ):
+        cat_tag = TagFactory(title="cat")
+        cat_advice = AdviceFactory(title="siam")
+        cat_advice.tags.add(cat_tag)
+
+        dog_tag = TagFactory(title="dog")
+        dog_advice = AdviceFactory(title="corky")
+        dog_advice.tags.add(dog_tag)
+
+        with subtests.test(msg=f"test_search_advice_by_tag_slug", i=cat_tag.title):
+            request = request_factory.get("/advices/", {"search": cat_tag.title})
+            force_authenticate(request, user=django_user)
+            response = advice_vs.as_view({"get": "list"})(request)
+
+            assert response.status_code == HTTPStatus.OK
+            assert response.data["count"] == 1
+            assert response.data["results"][0]["slug"] == cat_advice.slug
+
+        with subtests.test(msg=f"test_search_advice_by_tag_slug", i=dog_tag.title):
+            request = request_factory.get("/advices/", {"search": dog_tag.title})
+            force_authenticate(request, user=django_user)
+            response = advice_vs.as_view({"get": "list"})(request)
+
+            assert response.status_code == HTTPStatus.OK
+            assert response.data["count"] == 1
+            assert response.data["results"][0]["slug"] == dog_advice.slug
+
+        with subtests.test(msg=f"test_search_advice_by_tag_slug", i=user_tag.title):
+            request = request_factory.get("/advices/", {"search": user_tag.title})
+            force_authenticate(request, user=django_user)
+            response = advice_vs.as_view({"get": "list"})(request)
+
+            assert response.status_code == HTTPStatus.OK
+            assert response.data["count"] == 0
+
+    def test_search_advice_by_multiple_tags(
+        self,
+        db,
+        request_factory: APIRequestFactory,
+        django_user: get_user_model(),
+        advice: Advice,
+        advice_vs: AdviceViewSet,
+    ):
+        desk_tag = TagFactory(title="desk")
+        board_tag = TagFactory(title="board")
+        party_tag = TagFactory(title="party")
+
+        study_advice = AdviceFactory(title="Conspecting")
+
+        for tag in (desk_tag, board_tag):
+            study_advice.tags.add(tag)
+
+        request = request_factory.get(
+            "/advices/", {"search": f"{desk_tag.title} {party_tag.title}"}
+        )
+        force_authenticate(request, user=django_user)
+        response = advice_vs.as_view({"get": "list"})(request)
+
+        assert response.status_code == HTTPStatus.OK
+        assert response.data["count"] == 1
+        assert response.data["results"][0]["slug"] == study_advice.slug
