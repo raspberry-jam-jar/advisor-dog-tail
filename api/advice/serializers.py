f90ed6ce7ecb72e62187a824222c9b77e484a019
Maxim Yakovenko <yakovenko_me@km-union.ru>
2020-02-28 14:17:55 +0300

    Override create method in AdviceSerializer for m2m case handling.

--- api/advice/serializers.py
+++ api/advice/serializers.py
@@ -1,3 +1,5 @@
+import traceback
+
 from rest_framework import serializers
 from rest_framework.utils import model_meta
 
@@ -30,11 +32,71 @@ class AdviceSerializer(serializers.ModelSerializer):
         fields = ("title", "slug", "tags", "link", "created")
         read_only_fields = ("slug", "created")
 
-    def update_tags(sefl, manager, values: list):
+    def get_or_create_tags(sefl, manager, values: list):
         for row in values:
             tag, _ = Tag.objects.get_or_create(title=row["title"])
             manager.add(tag)
         return manager
+
+    def create(self, validated_data):
+        """
+        We have a bit of extra checking around this in order to provide
+        descriptive messages when something goes wrong, but this method is
+        essentially just:
+            return ExampleModel.objects.create(**validated_data)
+        If there are many to many fields present on the instance then they
+        cannot be set until the model is instantiated, in which case the
+        implementation is like so:
+            example_relationship = validated_data.pop('example_relationship')
+            instance = ExampleModel.objects.create(**validated_data)
+            instance.example_relationship = example_relationship
+            return instance
+        The default implementation also does not handle nested relationships.
+        If you want to support writable nested relationships you'll need
+        to write an explicit `.create()` method.
+        """
+        ModelClass = self.Meta.model
+
+        # Remove many-to-many relationships from validated_data.
+        # They are not valid arguments to the default `.create()` method,
+        # as they require that the instance has already been saved.
+        info = model_meta.get_field_info(ModelClass)
+        many_to_many = {}
+        for field_name, relation_info in info.relations.items():
+            if relation_info.to_many and (field_name in validated_data):
+                many_to_many[field_name] = validated_data.pop(field_name)
+
+        try:
+            instance = ModelClass._default_manager.create(**validated_data)
+        except TypeError:
+            tb = traceback.format_exc()
+            msg = (
+                "Got a `TypeError` when calling `%s.%s.create()`. "
+                "This may be because you have a writable field on the "
+                "serializer class that is not a valid argument to "
+                "`%s.%s.create()`. You may need to make the field "
+                "read-only, or override the %s.create() method to handle "
+                "this correctly.\nOriginal exception was:\n %s"
+                % (
+                    ModelClass.__name__,
+                    ModelClass._default_manager.name,
+                    ModelClass.__name__,
+                    ModelClass._default_manager.name,
+                    self.__class__.__name__,
+                    tb,
+                )
+            )
+            raise TypeError(msg)
+
+        # Save many-to-many relationships after the instance is created.
+        if many_to_many:
+            for field_name, value in many_to_many.items():
+                field = getattr(instance, field_name)
+                # if the field is an instance of a related manager which has Tag model
+                if field.model and field.model == Tag:
+                    field = self.get_or_create_tags(field, value)
+
+        return instance
 
     def update(self, instance, validated_data):
         info = model_meta.get_field_info(instance)
@@ -59,6 +121,6 @@ class AdviceSerializer(serializers.ModelSerializer):
             field = getattr(instance, attr)
             # if the field is an instance of a related manager which has Tag model
             if field.model and field.model == Tag:
-                field = self.update_tags(field, value)
+                field = self.get_or_create_tags(field, value)
 
         return instance
