336e4f8ea1ce83ed16cbc3ec701ad6116ab7cacd
Maxim Yakovenko <yakovenko_me@km-union.ru>
2020-02-28 14:18:35 +0300

    Add IsAuthenticated permission. Add tag fields for searching. Override SearchFilter. Add custom action for deleting an attached tag.

--- api/advice/viewsets.py
+++ api/advice/viewsets.py
@@ -1,8 +1,8 @@
-from rest_framework import mixins
-from rest_framework import viewsets
+from rest_framework import status, mixins, viewsets, permissions
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
-from rest_framework import filters
-
+from .filters import AdviceSearchFilter
 from .models import Advice
 from .serializers import AdviceSerializer
 
@@ -19,8 +19,31 @@ class AdviceViewSet(
     """
 
     queryset = Advice.objects.order_by("-created").all()
+    permission_classes = (permissions.IsAuthenticated,)
     serializer_class = AdviceSerializer
-    filter_backends = (filters.SearchFilter,)
-    search_fields = ("title",)
+    filter_backends = (AdviceSearchFilter,)
+    search_fields = (
+        "title",
+        "tags__title",
+        "tags__slug",
+    )
     ordering_fields = ("title", "created")
     ordering = "-created"
+
+    @action(
+        detail=True,
+        methods=["delete"],
+        url_path=r"tag/(?P<tag_slug>\w{1})",
+        permission_classes=(permissions.IsAuthenticated,),
+    )
+    def tag(self, request, tag_slug, pk=None):
+        """
+        Remove tag attached to the advice.
+        """
+        advice = self.get_object()
+        rel_tag_qs = advice.tags.filter(slug=tag_slug)
+        if rel_tag_qs.exists():
+            rel_tag_qs.first().delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        else:
+            return Response(status=status.HTTP_404_NOT_FOUND)
