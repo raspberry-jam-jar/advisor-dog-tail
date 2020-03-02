93157e1fe3eea7eaaeb5400de1708a311e0816a2
Maxim Yakovenko <yakovenko_me@km-union.ru>
2020-03-02 12:23:02 +0300

    Reffactor tests for Account model serializer.

--- api/users/tests/test_serializers.py
+++ api/users/tests/test_serializers.py
@@ -9,21 +9,21 @@ from .factories import AccountFactory
 
 
 @pytest.mark.django_db
-class TestClientSerializer:
-    def test_add_client(self, db):
+class TestAccountSerializer:
+    def test_add_account(self, db):
         client = AccountFactory.build()
         serializer = AccountSerializer(data=model_to_dict(client))
         assert serializer.is_valid()
-        model = serializer.create(serializer.validated_data)
-        assert Account.objects.filter(email=model.email).exists()
+        serializer.save()
+        assert Account.objects.filter(email=client.email).exists()
 
-    def test_add_client_duplicate(self, db):
+    def test_add_account_duplicate(self, db):
         account = AccountFactory()
         serializer = AccountSerializer(data=model_to_dict(account))
         assert not serializer.is_valid()
         assert "email" in serializer.errors.keys()
 
-    def test_read_client(self, db):
+    def test_read_account(self, db):
         client = AccountFactory()
         serializer = AccountSerializer(client)
         assert {"pk", "email"} == set(serializer.data.keys())
