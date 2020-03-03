a1601754f6bf98cbc3adc23726fe2050696c3092
Maxim Yakovenko <yakovenko_me@km-union.ru>
2020-03-03 12:05:50 +0300

    Fix and adjust tests for Advice serializers.

--- api/advice/tests/test_serializers.py
+++ api/advice/tests/test_serializers.py
@@ -1,10 +1,9 @@
 import pytest
 
-from django.forms.models import model_to_dict
+from model_bakery import baker
 
-from ..serializers import AdviceSerializer
 from ..models import Advice
-
+from ..serializers import AdviceSerializer, UpdateAdviceSerializer
 from .factories import AdviceFactory
 
 
@@ -13,43 +12,51 @@ from .factories import AdviceFactory
 class TestAdviceSerializer:
     def test_create_advice(self, db):
         advice = AdviceFactory.build()
-        serializer = AdviceSerializer(data=model_to_dict(advice))
+        advice_data = {
+            "title": advice.title,
+            "link": advice.link,
+            "author": {"email": advice.author.email},
+        }
+        serializer = AdviceSerializer(data=advice_data)
         serializer.is_valid(raise_exception=True)
-        model = serializer.save()
-        assert Advice.objects.filter(slug=model.slug).exists()
+        serializer.save()
+        assert Advice.objects.filter(title=advice.title).exists()
 
     def test_update_advice(self, subtests, db):
         advice = AdviceFactory()
         new_advice = AdviceFactory.build()
         assert not Advice.objects.filter(title=new_advice.title).exists()
-        serializer = AdviceSerializer(
+        serializer = UpdateAdviceSerializer(
             advice, data={"title": new_advice.title, "link": new_advice.link}
         )
         serializer.is_valid(raise_exception=True)
         serializer.save()
-        assert Advice.objects.filter(
+        assert not Advice.objects.filter(
             title=new_advice.title, link=new_advice.link
         ).exists()
+        assert Advice.objects.filter(title=new_advice.title).exists()
 
     def test_partial_update_advice(self, subtests, db):
-        new_advice = AdviceFactory.build()
-        for field in ("title", "link"):
-            with subtests.test(msg=f"test_edit_{field}"):
-                advice = AdviceFactory()
-                value = getattr(new_advice, field)
-                serializer = AdviceSerializer(advice, data={field: value}, partial=True)
+        new_advice = baker.prepare(Advice)
+        for field in ("title",):
+            with subtests.test(msg=f"test_edit_{field}", i=field):
+                advice = baker.make(Advice)
+                field_data = {field: getattr(new_advice, field)}
+                serializer = UpdateAdviceSerializer(
+                    advice, data=field_data, partial=True
+                )
                 serializer.is_valid(raise_exception=True)
                 serializer.save()
-                assert Advice.objects.filter(**{field: value}).exists()
+                assert Advice.objects.filter(**field_data).exists()
 
-        # Editing slug field should be forbidden
-        with subtests.test(msg="test_edit_slug"):
-            advice = AdviceFactory()
-            serializer = AdviceSerializer(
-                advice, data={"slug": new_advice.slug}, partial=True
-            )
-            serializer.is_valid(raise_exception=True)
-            serializer.save()
-            assert not Advice.objects.filter(
-                title=advice.title, slug=new_advice.slug
-            ).exists()
+        # Editing should be forbidden
+        for field in ("link", "slug", "author"):
+            with subtests.test(msg=f"test_edit_{field}", i=field):
+                advice = baker.make(Advice)
+                field_data = {field: getattr(new_advice, field)}
+                serializer = UpdateAdviceSerializer(
+                    advice, data=field_data, partial=True
+                )
+                serializer.is_valid(raise_exception=True)
+                serializer.save()
+                assert not Advice.objects.filter(**field_data).exists()
