558a8a4a535c3867209e10cf169ee7d5708552f2
Maxim Yakovenko <yakovenko_me@km-union.ru>
2020-03-03 16:01:02 +0300

    Add natural key to TagType model. Add Tag model manager and add the manager method for getting a default tag.

--- api/advice/models/tag.py
+++ api/advice/models/tag.py
@@ -9,6 +9,9 @@ from .utils import get_default_tag_type
 
 
 class TagTypeManger(models.Manager):
+    def get_by_natural_key(self, slug):
+        return self.get(slug=slug)
+
     def default(self):
         return self.get_queryset().filter(slug="prochee").get()
 
@@ -17,12 +20,20 @@ class TagType(UUIDModel, TitleSlugModel, TimeStampedModel):
 
     objects = TagTypeManger()
 
-    def __str__(self):
-        return self.title
-
     class Meta:
         verbose_name = _("Tag type")
         verbose_name_plural = _("Tag types")
+
+    def natural_key(self):
+        return (self.slug,)
+
+    def __str__(self):
+        return self.title
+
+
+class TagManager(models.Manager):
+    def default(self):
+        return self.get_queryset().filter(slug="prochee").get()
 
 
 class Tag(UUIDModel, TitleSlugModel, TimeStampedModel):
@@ -30,6 +41,8 @@ class Tag(UUIDModel, TitleSlugModel, TimeStampedModel):
     type = models.ForeignKey(
         TagType, default=get_default_tag_type, on_delete=models.SET_DEFAULT
     )
+
+    objects = TagManager()
 
     def __str__(self):
         return self.title
