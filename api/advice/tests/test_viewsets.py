285e9c0a314627b4b2145b7c7789cfac1d738a53
Maxim Yakovenko <yakovenko_me@km-union.ru>
2020-03-03 12:42:28 +0300

    Add test for author field. Reffactor tests for advice searching using Russian.

--- api/advice/tests/test_viewsets.py
+++ api/advice/tests/test_viewsets.py
@@ -24,8 +24,12 @@ class TestAdviceViewset:
         advice_vs: AdviceViewSet,
     ):
         advice = AdviceFactory.build()
-        advice_data = {"title": advice.title, "link": advice.link}
-        request = request_factory.post("/advices/", advice_data)
+        advice_data = {
+            "title": advice.title,
+            "link": advice.link,
+            "author": {"email": advice.author.email},
+        }
+        request = request_factory.post("/advices/", advice_data, format="json")
         force_authenticate(request, user=django_user)
         response = advice_vs.as_view({"post": "create"})(request)
         assert response.status_code == HTTPStatus.CREATED
@@ -46,6 +50,7 @@ class TestAdviceViewset:
         force_authenticate(request, user=django_user)
         response = advice_vs.as_view({"put": "update"})(request, pk=advice.pk)
         assert response.status_code == HTTPStatus.OK
+        assert not Advice.objects.filter(slug=advice.slug).exists()
         assert Advice.objects.filter(title=new_advice.title).exists()
 
     def test_partial_update_advice(
@@ -57,22 +62,19 @@ class TestAdviceViewset:
         advice_vs: AdviceViewSet,
     ):
         new_advice = baker.prepare(Advice)
-        for field in ("title", "link"):
-            with subtests.test(msg=f"test_update_{field}"):
+        for field in ("title",):
+            with subtests.test(msg=f"test_update_{field}", i=field):
                 advice = baker.make(Advice)
-                value = getattr(new_advice, field)
-                request = request_factory.patch(
-                    f"/advices/{advice.pk}/", {field: value}
-                )
+                field_data = {field: getattr(new_advice, field)}
+                request = request_factory.patch(f"/advices/{advice.pk}/", field_data)
                 force_authenticate(request, user=django_user)
                 response = advice_vs.as_view({"patch": "partial_update"})(
                     request, pk=advice.pk
                 )
-                lookup_data = {field: value}
                 assert response.status_code == HTTPStatus.OK
-                assert Advice.objects.filter(**lookup_data).exists()
+                assert Advice.objects.filter(**field_data).exists()
 
-    def test_search_advice(
+    def test_search_advice_using_ru(
         self,
         db,
         subtests,
@@ -81,12 +83,12 @@ class TestAdviceViewset:
         request_factory: APIRequestFactory,
         advice_vs: AdviceViewSet,
     ):
-        usefull_advice = AdviceFactory(
-            title="Полезная ссылка", link="https://www.google.com/"
+        usefull_advice = baker.make(
+            Advice, title="Полезная ссылка", link="https://www.google.com/"
         )
-        usefull_advice.save()
-        advice = AdviceFactory(title="Так себе", link="https://www.rambler.ru/")
-        advice.save()
+        advice = baker.make(  # noqa: F841
+            Advice, title="Так себе", link="https://www.rambler.ru/"
+        )
 
         request = request_factory.get("/advices/", {"search": "Полез"})
         force_authenticate(request, user=django_user)
